96b8bb6c3d474db4a02badd29a6bf2d59c4d0337
Nguyen Trong Phuc <nguyentrongphuc1804@gmail.com>
2023-12-02 21:42:06 +0700

    fix link

--- src/util/setting/config.js
+++ src/util/setting/config.js
@@ -1,4 +1,4 @@
-export const DOMAIN = "https://movieapi.cyberlearn.vn/api/";
+export const DOMAIN = process.env.PUBLIC_URL;
 export const ACCESS_TOKEN = "accessToken";
 export const USER_INFO = "USER_INFO";
 export const groupId = "GP00";
